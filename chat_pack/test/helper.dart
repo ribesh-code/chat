22965e2101923ab549a4e2014a9ffb287f0c4cb8
Ribesh Basnet <badribesh@gmail.com>
2022-02-18 22:04:36 +0545

    created helper for testing.

---
+++ chat_pack/test/helper.dart
@@ -0,0 +1,12 @@
+import 'package:rethink_db_ns/rethink_db_ns.dart';
+
+Future<void> createDB(RethinkDb r, Connection c) async {
+  await r.dbCreate('test').run(c).catchError((e) => {});
+  await r.tableCreate('users').run(c).catchError((e) => {});
+  await r.tableCreate('messages').run(c).catchError((e) => {});
+}
+
+Future<void> cleanDB(RethinkDb r, Connection c) async {
+  await r.table('users').delete().run(c);
+  await r.table('messages').delete().run(c);
+}
