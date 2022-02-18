4cfbc210953d6e8e57caa1ef885af20c8ee53157
Ribesh Basnet <badribesh@gmail.com>
2022-02-18 22:45:20 +0545

    updated test helper class.

--- chat_pack/test/helper.dart
+++ chat_pack/test/helper.dart
@@ -4,9 +4,11 @@ Future<void> createDB(RethinkDb r, Connection c) async {
   await r.dbCreate('test').run(c).catchError((e) => {});
   await r.tableCreate('users').run(c).catchError((e) => {});
   await r.tableCreate('messages').run(c).catchError((e) => {});
+  await r.tableCreate('receipts').run(c).catchError((e) => {});
 }
 
 Future<void> cleanDB(RethinkDb r, Connection c) async {
   await r.table('users').delete().run(c);
   await r.table('messages').delete().run(c);
+  await r.table('receipts').delete().run(c);
 }
